a825bed3e76d7bc3765b5cd289b86a15ad71d723
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-02-17 14:48:37 +0000

    datatypes_summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,45 @@
+// Primitive Datatypes
+
+/*
+1. Number
+2.String
+3.Boolean
+4.Null
+5.undefined
+6.Symbol
+7.BigInt
+*/
+
+// const score = 100; 
+// Dynamically typed
+
+// Reference or non Primitive
+
+/*
+Array
+Objects
+Functions
+*/
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId); //False
+
+
+const heros = ["Shaktiman","Naagraj","Doga"];
+
+let myobj ={
+    name:"Swayam",
+    age:22,
+}
+
+ const myfunction = function(){
+    console.log("Hello");
+
+}
+
+const bigNumber = 34556788986543n;
+
+// Null ka typeof object hota hai
+console.log(typeof null);
