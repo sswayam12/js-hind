8a6c37aaabb43657fd17ee5438778ac209cb4a10
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-02-17 13:26:11 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1444567;
+let accountEmail ="swayam@google.com";
+var accountPassword ="12345";
+accountCity ="Jaipur";
+
+// accountId ="qwerty";
+console.log(accountId);
+
+accountEmail="Swayam@grewaq.com";
+console.log(accountEmail);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+// Prefer not use var 
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+// aler(3+3);  We are using nodejs not browsers
+
+console.log(3+3);
+console.log("Swayam");
+
+// Code readability should be high
+
+let name = "Swayam";
+let age  = 18;
+let isLoggedIn = false;
+let state; //undefined;
+let sate = null;
+
+// Number =>2 to the power of 53
+// bigInt
+// String =>" "
+// Boolean =>True and false
+// Null =>standalone value
+// undefined =>
+// Symbol
+// Object 
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
