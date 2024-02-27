dfc5c001d30dc916ca3b06e05e4f7e5b22f8745c
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-02-27 12:09:44 +0000

    scopes2

---
+++ .devcontainer/03_basics/scopes2.js
@@ -0,0 +1,21 @@
+function one(){
+    const userNAme ="Swayam";
+    consol
+
+    function two(){
+        const website ="Youtube";
+        console.log(userNAme);
+    }
+    // console.log(website);
+
+    // two();
+}
+one();
+
+
+
+
+function addOne(num){
+    return num + 1;
+}
+
