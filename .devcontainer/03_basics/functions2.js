10649beb919681f674f852a203e48b0ed0d62682
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-02-27 11:48:10 +0000

    functions2

---
+++ .devcontainer/03_basics/functions2.js
@@ -0,0 +1,24 @@
+function calculateCartPrice(val1,val2,...num1){
+    return num1;
+}
+console.log(calculateCartPrice(200,400,500,2000,3400));
+
+const user ={
+    userName:"Swayam",
+    price:199
+}
+
+function handleObject(anyObject){
+    // console.log(`UserName is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+
+handleObject(user);
+
+const myNewArray =[200,400,600,700];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
+
