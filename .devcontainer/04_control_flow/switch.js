db4b74d7cea7e38639c9ec276afb84b0e4277006
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-02-28 02:55:13 +0000

    switch

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,24 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+         console.log("February");
+         break;
+    case 3:
+        console.log("March");
+        break;     
+    default:
+        break;
+}
