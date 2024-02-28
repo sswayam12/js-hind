1762f89bda222340da134e09af26fc89c623f208
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-02-28 01:42:44 +0000

    one

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,29 @@
+// 
+
+const isUserLoggedIn = true;
+if(isUserLoggedIn){
+
+}
+// if(2 == "2"){
+//     console.log("Executed");
+
+// }
+// > , < , >=, <=, ==, ===
+
+// const score = 200;
+// if(score>100){
+//     const power ="fly";
+//     console.log(`User power: ${power}`);
+// }
+
+// SHORTHAND NOTATION
+const balance = 743;
+
+if(balance > 500) console.log("test");
+
+if(balance > 500){
+    console.log("less than 750");
+}
+else if(balance > 750){
+    console.log("less than 1000");
+}
